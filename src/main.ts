7023bffe742fbf93bfcc46bc79ed2560c11e6e19
Spencer Pope <spenceropope@gmail.com>
2022-03-14 18:37:26 -0400

    Add supabase

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,32 @@
+import { createClient } from '@supabase/supabase-js'
 import './style.css'
+
+const supabaseUrl = 'https://moqzweyzqfmaujwqzjje.supabase.co'
+const supabaseKey = import.meta.env.VITE_SUPABASE_KEY as string;
+const supabase = createClient(supabaseUrl, supabaseKey)
+console.log(supabase)
 
 const app = document.querySelector<HTMLDivElement>('#app')!
 
 app.innerHTML = `
-  <h1>Hello Vite!</h1>
-  <a href="https://vitejs.dev/guide/features.html" target="_blank">Documentation</a>
+  <h1>Recipes</h1>
 `
+
+const main = async () => {
+
+  const getData = async () => {
+    let { data, error } = await supabase
+      .from('recipes')
+      .select('title')
+
+    return data;
+  }
+
+  const data = await getData();
+
+  console.log({ data })
+
+  app.innerHTML += data?.map(recipe => `<div>${recipe.title}</div>`).join('');
+}
+
+main();
